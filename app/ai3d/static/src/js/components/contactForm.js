d3c53d56d27ffc05e69a6e3f4d1752269d617f0c
AnetaRolnik <aneta.r@poczta.onet.eu>
2018-10-29 16:37:08 +0100

    Show errors when validating the contact form

--- app/ai3d/static/src/js/components/contactForm.js
+++ app/ai3d/static/src/js/components/contactForm.js
@@ -28,6 +28,7 @@ function contact() {
         }
     });
 
+
     $(".contact-form").on("submit", function(event){
         event.preventDefault();
 
@@ -46,29 +47,45 @@ function contact() {
 
         const regexName = /^[a-zA-Z]{2,20}$/;
         const regexEmail = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        const errors = [];
+
+        if ($(".contact-form-errors") !== null) {
+            $(".contact-form-errors").remove();
+        }
 
         if (!regexName.test(nameVal)) {
             name.css("border","1px solid red");
+            errors.push('Imię - wymagane min 2 znaki.');
         } else {
             name.css("border","1px solid #ccc");
         }
 
         if (!regexName.test(surnameVal)) {
             surname.css("border","1px solid red");
+            errors.push('Nazwisko - wymagane min 2 znaki.');
         } else {
             surname.css("border","1px solid #ccc");
         }
 
-        if (!regexEmail.test(regexEmail)) {
+        if (!regexEmail.test(emailVal)) {
             email.css("border","1px solid red");
         } else {
             email.css("border","1px solid #ccc")
         }
 
-        if (messageVal.length <= 8 || messageVal.length >= 200) {
+        if (messageVal.length < 8 || messageVal.length > 200) {
             message.css("border","1px solid red");
+            errors.push("Wiadomość - wymagane min 8 znaków.");
         } else {
             message.css("border","1px solid #ccc");
+        }
+
+        if (errors.length > 0) {
+            const $errorContainer = $("<div class='contact-form-errors'><span><b>Niepoprawne dane!</b></span></div>");
+            $.map(errors, function(error) {
+                return $errorContainer.append("<span>"+error+"</span>");
+            });
+            btn.after( $errorContainer );
         }
 
         if (regexName.test(nameVal) && regexName.test(surnameVal) && regexEmail.test(emailVal) && messageVal.length>=8 && messageVal.length<=200) {
@@ -84,10 +101,10 @@ function contact() {
             }).done(function(){
                 //clear fields
                 fields.val("");
-                fields.css("border","1px solid #ccc")
+                fields.css("border","1px solid #ccc");
 
                 //add information
-                const $info = $("<span class='contact-form-info'>Wiadomość została wysłana</span>");
+                const $info = $("<span class='contact-form-info'>Wiadomość została wysłana.</span>");
                 btn.after( $info );
 
                 //change style btn
