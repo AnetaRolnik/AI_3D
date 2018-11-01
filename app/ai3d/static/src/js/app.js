70e896e31cb670b837aa0c375f2c5e1b05d5e90a
AnetaRolnik <aneta.r@poczta.onet.eu>
2018-11-01 16:57:31 +0100

    Set the height of sections using $(window).width()

--- app/ai3d/static/src/js/app.js
+++ app/ai3d/static/src/js/app.js
@@ -1,10 +1,14 @@
-import map from "./components/map";
+import setHeight from "./components/setHeight";
 import scrollNav from "./components/scrollNav";
+import map from "./components/map";
 import contact from "./components/contactForm";
 
 document.addEventListener("DOMContentLoaded", function(){
-    map();
+    setHeight();
     scrollNav();
+    map();
     contact();
 });
+
+
 
