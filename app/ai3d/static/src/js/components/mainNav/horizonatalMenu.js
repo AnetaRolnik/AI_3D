9d0f33592ee40e61e8917c86fe976558d0104f48
AnetaRolnik <aneta.r@poczta.onet.eu>
2018-12-03 20:16:37 +0100

    Make responsive web design for mobile

--- app/ai3d/static/src/js/components/mainNav/horizonatalMenu.js
+++ app/ai3d/static/src/js/components/mainNav/horizonatalMenu.js
@@ -1,7 +1,7 @@
 function horizontalMenu() {
     const nav = $('.main-nav');
 
-    if ($(window).width() < 1140 && $(window).width() > 720) {
+    if ($(window).width() <= 1140 && $(window).width() > 720) {
         $(window).on('scroll', function () {
             if (window.scrollY > 100) {
                 nav.addClass('nav-fixed');
