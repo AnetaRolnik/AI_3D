aa5ab5288f0a9f265cc652046638f153db317066
AnetaRolnik <aneta.r@poczta.onet.eu>
2019-06-28 22:25:17 +0200

    form in progress

--- app/ai3d/static/src/js/components/training.js
+++ app/ai3d/static/src/js/components/training.js
@@ -1,127 +1,153 @@
 function register() {
-    const trainingSelect = $(".training-select");
-    const firstOption = trainingSelect.find('option:first-child')[0].value;
-
-    function getDate(url) {
-        const dateSelect = $(".date-select");
-        $.ajax({
-            url: url,
-        }).done(function (res) {
-            dateSelect.children().length>0 ? dateSelect.empty() : null;
-            $.map(res, function (el) {
-                return dateSelect.append("<option id="+el.id+">" + el.data + "</option>");
-            });
-        });
-    }
-
-    getDate("training/"+firstOption);
-
-    trainingSelect.change(function() {
-        const selectVal = $(this).find('option:selected').val();
-        getDate("training/"+selectVal);
+    const addParticipant = $("#addParticipant");
+    addParticipant.on("click", function() {
+        addParticipant.before(`
+        <div class="registration-form-row">
+            <label class="registration-form-label" for="participantName">Imię i nazwisko</label>
+            <input class="registration-form-input" type="text" name="name" id="participantName">
+        </div>
+        `);
     });
 
+    const invoiceCheckbox = $("#invoice");
+    const invoiceData = $("#invoiceData");
+    invoiceCheckbox.on("click", function() {
+        invoiceCheckbox.prop('checked') ? invoiceData.css('display', 'block') : invoiceData.css('display', 'none');
+    });    
 
-    $(".training-registration-form").on("submit", function(event) {
-        event.preventDefault();
-        const name = $('#userName'),
-            email = $('#userEmail'),
-            phone = $('#userTel'),
-            fields = $('.registration-form-input'),
+    const userCheckbox = $("#user");
+    const participantInput = $("#participantsData .required");
+    userCheckbox.on("click", function() {
+        const nameVal = $("#personalData #name").val();
+        if(userCheckbox.prop('checked')) {
+            participantInput.val(String(nameVal)).prop('readonly', true).css('background', '#f6f7f6');
+        } else { 
+            participantInput.val('')
+        };
+    });    
 
-            nameVal = name.val(),
-            emailVal = email.val(),
-            phoneVal = phone.val(),
-            optionId = $('.date-select option:selected')[0].id,
+    // const trainingSelect = $(".training-select");
+    // const firstOption = trainingSelect.find('option:first-child')[0].value;
 
-            btn = $('.registration-form-btn'),
-            containerBtn = $('.registration-btn-container'),
+    // function getDate(url) {
+    //     const dateSelect = $(".date-select");
+    //     $.ajax({
+    //         url: url,
+    //     }).done(function (res) {
+    //         dateSelect.children().length>0 ? dateSelect.empty() : null;
+    //         $.map(res, function (el) {
+    //             return dateSelect.append("<option id="+el.id+">" + el.data + "</option>");
+    //         });
+    //     });
+    // }
 
-            //validation form
-            regexEmail = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-            regexPhone =  /^[0-9]{6,12}$/;
+    // getDate("training/"+firstOption);
 
-        if (nameVal==='') {
-            name.next().remove();
-            name.after($("<span class='tooltiptext'>Uzupełnij pole</span>"));
-            name.parent().addClass('tooltip');
-            name.css("borderColor","red");
-        } else {
-            name.next().remove();
-            name.parent().removeClass('tooltip');
-            name.css("borderColor","#ccc");
-        }
+    // trainingSelect.change(function() {
+    //     const selectVal = $(this).find('option:selected').val();
+    //     getDate("training/"+selectVal);
+    // });
 
-        if (emailVal==='') {
-            email.next().remove();
-            email.after($("<span class='tooltiptext'>Uzupełnij pole</span>"));
-            email.parent().addClass('tooltip');
-            email.css("borderColor", "red");
-        } else if (emailVal!=='' && !regexEmail.test(emailVal)) {
-            email.next().remove();
-            email.after($("<span class='tooltiptext'>Wpisz poprawny email</span>"));
-            email.parent().addClass('tooltip');
-            email.css("borderColor", "red");
-        } else {
-            email.next().remove();
-            email.parent().removeClass('tooltip');
-            email.css("borderColor","#ccc");
-        }
+    // $(".training-registration-form").on("submit", function(event) {
+    //     event.preventDefault();
+    //     const name = $('#userName'),
+    //         email = $('#userEmail'),
+    //         phone = $('#userTel'),
+    //         fields = $('.registration-form-input'),
 
-        if (phoneVal==='') {
-            phone.next().remove();
-            phone.after($("<span class='tooltiptext'>Uzupełnij pole</span>"));
-            phone.parent().addClass('tooltip');
-            phone.css("borderColor", "red");
-        } else if (phoneVal!=='' && !regexPhone.test(phoneVal)) {
-            phone.next().remove();
-            phone.after($("<span class='tooltiptext'>Wpisz poprawny numer</span>"));
-            phone.parent().addClass('tooltip');
-            phone.css("borderColor", "red");
-        } else {
-            phone.next().remove();
-            phone.parent().removeClass('tooltip');
-            phone.css("borderColor","#ccc");
-        }
+    //         nameVal = name.val(),
+    //         emailVal = email.val(),
+    //         phoneVal = phone.val(),
+    //         optionId = $('.date-select option:selected')[0].id,
+
+    //         btn = $('.registration-form-btn'),
+    //         containerBtn = $('.registration-btn-container'),
+
+    //         //validation form
+    //         regexEmail = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+    //         regexPhone =  /^[0-9]{6,12}$/;
+
+    //     if (nameVal==='') {
+    //         name.next().remove();
+    //         name.after($("<span class='tooltiptext'>Uzupełnij pole</span>"));
+    //         name.parent().addClass('tooltip');
+    //         name.css("borderColor","red");
+    //     } else {
+    //         name.next().remove();
+    //         name.parent().removeClass('tooltip');
+    //         name.css("borderColor","#ccc");
+    //     }
+
+    //     if (emailVal==='') {
+    //         email.next().remove();
+    //         email.after($("<span class='tooltiptext'>Uzupełnij pole</span>"));
+    //         email.parent().addClass('tooltip');
+    //         email.css("borderColor", "red");
+    //     } else if (emailVal!=='' && !regexEmail.test(emailVal)) {
+    //         email.next().remove();
+    //         email.after($("<span class='tooltiptext'>Wpisz poprawny email</span>"));
+    //         email.parent().addClass('tooltip');
+    //         email.css("borderColor", "red");
+    //     } else {
+    //         email.next().remove();
+    //         email.parent().removeClass('tooltip');
+    //         email.css("borderColor","#ccc");
+    //     }
+
+    //     if (phoneVal==='') {
+    //         phone.next().remove();
+    //         phone.after($("<span class='tooltiptext'>Uzupełnij pole</span>"));
+    //         phone.parent().addClass('tooltip');
+    //         phone.css("borderColor", "red");
+    //     } else if (phoneVal!=='' && !regexPhone.test(phoneVal)) {
+    //         phone.next().remove();
+    //         phone.after($("<span class='tooltiptext'>Wpisz poprawny numer</span>"));
+    //         phone.parent().addClass('tooltip');
+    //         phone.css("borderColor", "red");
+    //     } else {
+    //         phone.next().remove();
+    //         phone.parent().removeClass('tooltip');
+    //         phone.css("borderColor","#ccc");
+    //     }
 
 
-        if (nameVal!=='' && regexEmail.test(emailVal) && regexPhone.test(phoneVal)) {
-            $.ajax({
-                url: "training",
-                method: "POST",
-                data: {
-                    name: nameVal,
-                    email: emailVal,
-                    phone_number: phoneVal,
-                    id: optionId,
-                }
-            }).done(function () {
-                //clear fields
-                fields.val('');
-                fields.css("borderColor", "#ccc");
+    //     if (nameVal!=='' && regexEmail.test(emailVal) && regexPhone.test(phoneVal)) {
+    //         $.ajax({
+    //             url: "training",
+    //             method: "POST",
+    //             data: {
+    //                 name: nameVal,
+    //                 email: emailVal,
+    //                 phone_number: phoneVal,
+    //                 id: optionId,
+    //             }
+    //         }).done(function () {
+    //             //clear fields
+    //             fields.val('');
+    //             fields.css("borderColor", "#ccc");
 
-                //add information
-                $('.registration-form-state').remove();
-                const $state = $("<p class='registration-form-state'>Dziękujemy za zapisanie się na szkolenie</p>");
-                containerBtn.append($state);
+    //             //add information
+    //             $('.registration-form-state').remove();
+    //             const $state = $("<p class='registration-form-state'>Dziękujemy za zapisanie się na szkolenie</p>");
+    //             containerBtn.append($state);
 
-                //change style btn
-                btn.addClass('registration-btn-success').prop('disabled', true);
+    //             //change style btn
+    //             btn.addClass('registration-btn-success').prop('disabled', true);
 
-                setTimeout(function(){
-                    btn.removeClass('registration-btn-success').prop('disabled', false);
-                    $('.registration-form-state').remove();
-                },7000);
+    //             setTimeout(function(){
+    //                 btn.removeClass('registration-btn-success').prop('disabled', false);
+    //                 $('.registration-form-state').remove();
+    //             },7000);
 
-            }).fail(function () {
-                //add information
-                if ($('.registration-form-state').length === 0) {
-                    const $state = $("<p class='registration-form-state'>Zapisanie nie powiodło się. Spróbuj ponownie za chwilę</p>");
-                    containerBtn.append($state);
-                }
-            });
-        }
-    });
+    //         }).fail(function () {
+    //             //add information
+    //             if ($('.registration-form-state').length === 0) {
+    //                 const $state = $("<p class='registration-form-state'>Zapisanie nie powiodło się. Spróbuj ponownie za chwilę</p>");
+    //                 containerBtn.append($state);
+    //             }
+    //         });
+    //     }
+    // });
 }
 
 export default register;
