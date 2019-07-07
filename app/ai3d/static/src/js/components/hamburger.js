5f45375d6862b5c164a8aeddc6bb55b92c6d56b9
AnetaRolnik <aneta.r@poczta.onet.eu>
2019-07-07 18:16:37 +0200

    Add social media and small fixes

--- app/ai3d/static/src/js/components/hamburger.js
+++ app/ai3d/static/src/js/components/hamburger.js
@@ -1,15 +1,16 @@
 function hamburger() {
     const btn = $('.nav-toggle');
-    const list = $('.nav-list');
+    const nav = $('.nav');
+    const header = $('header');
 
     btn.on('click', function(e) {
         e.preventDefault();
-        list.toggleClass('show');
+        nav.toggleClass('show');
         btn.toggleClass('anim');
     });
 
     const hamburger = $('.nav-toggle span');
-    const heightHero = $('.home').height();
+    const heightHero = $('.banner').height();
 
     $(window).on('scroll', _.debounce(function(){
         scrollY > heightHero ? hamburger.css('background', '#5fc9c9') : hamburger.css('background', 'white');
