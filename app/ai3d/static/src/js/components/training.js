5b646900fdd1a7a7b8eb545cd94fb7d556fc666e
AnetaRolnik <aneta.r@poczta.onet.eu>
2019-07-16 20:27:01 +0200

    Submit form using serializeJson

--- app/ai3d/static/src/js/components/training.js
+++ app/ai3d/static/src/js/components/training.js
@@ -12,21 +12,22 @@ function training() {
         trainingSelect = $(".training-select"),
         firstOption = trainingSelect.find("option:first-child")[0].value;
 
-    const participant = (`
-        <li class="participantData" data-aos="fade-up" data-aos-duration="500" data-aos-delay="50" data-aos-once="true">
-            <div class="registration-form-row">
-                <label class="registration-form-label" for="participantName">Imię i nazwisko</label>
-                <input class="registration-form-input" type="text" name="name" id="participantName">
-            </div>
-            <div class="registration-form-row">
-                <label class="registration-form-label" for="participantEmail">Adres email</label>
-                <input class="registration-form-input" name="email" id="participantEmail">
-            </div>
-        </div>
-    `)
+    let  i = 0;
 
     addParticipant.on("click", function() {
-        participantsData.append(participant);
+        i++;
+        participantsData.append(`
+            <li class="participantData" data-aos="fade-up" data-aos-duration="500" data-aos-delay="50" data-aos-once="true">
+                <div class="registration-form-row">
+                    <label class="registration-form-label" for="participantName">Imię i nazwisko</label>
+                    <input class="registration-form-input" type="text" name="participants[${i}][name]" id="participantName">
+                </div>
+                <div class="registration-form-row">
+                    <label class="registration-form-label" for="participantEmail">Adres email</label>
+                    <input class="registration-form-input" name="participants[${i}][email]" id="participantEmail">
+                </div>
+            </div>
+        `);
         participantsData.children().length === 5 ? $(this).remove() : null;
     });
 
@@ -53,23 +54,23 @@ function training() {
             <h3 class="registartion-form-title">Dane instytucji zgłaszającej (dane do faktury)</h3>
             <div class="registration-form-row">
                 <label class="registration-form-label" for="companyName">Nazwa firmy instytucji *</label>
-                <input class="registration-form-input required" type="text" name="name" id="companyName">
+                <input class="registration-form-input required" type="text" name="invoice[institution_name]" id="companyName">
             </div>
             <div class="registration-form-row">
                 <label class="registration-form-label" for="companyAddress">Adres siedziby *</label>
-                <input class="registration-form-input required" type="text" name="address" id="companyAddress">
+                <input class="registration-form-input required" type="text" name="invoice[institution_address]" id="companyAddress">
             </div>
             <div class="registration-form-row">
                 <label class="registration-form-label" for="companyTel">Telefon komórkowy *</label>
-                <input class="registration-form-input required" type="tel" placeholder="Tylko cyfry np.500500500" name="tel" id="companyTel">
+                <input class="registration-form-input required" type="tel" name="invoice[phone_number]" placeholder="Tylko cyfry np.500500500" id="companyTel">
             </div>
             <div class="registration-form-row">
                 <label class="registration-form-label" for="companyEmail">Adres email *</label>
-                <input class="registration-form-input required" name="email" id="companyEmail">
+                <input class="registration-form-input required" name="invoice[email]" id="companyEmail">
             </div>
             <div class="registration-form-row">
                 <label class="registration-form-label" for="companyNIP">NIP</label>
-                <input class="registration-form-input" type="number" name="nip" id="companyNIP">
+                <input class="registration-form-input" type="number" name="invoice[nip]" id="companyNIP">
             </div>
         </div>
     `)
@@ -86,17 +87,22 @@ function training() {
             url: url,
         }).done(function (res) {
             dateSelect.children().length>0 ? dateSelect.empty() : null;
-            $.map(res, function (el) {
-                return dateSelect.append(`<option id=${el.id}> ${el.data} </option>`);
-            });
+
+            if (res.length === 0) {
+                return dateSelect.append(`<option>Nie ma terminów. Wróć później lub napisz do nas</option>`);
+            } else {
+                $.map(res, function (el) {
+                    return dateSelect.append(`<option value="${el.id}"> ${el.date} </option>`);
+                });
+            }
         });
     }
 
-    getDate("training/"+firstOption);
+    getDate("trainings/"+firstOption);
 
     trainingSelect.change(function() {
         const selectVal = $(this).find("option:selected").val();
-        getDate("training/"+selectVal);
+        getDate("trainings/"+selectVal);
     });
 
     
@@ -107,44 +113,22 @@ function training() {
 
         e.preventDefault();
         validation(requiredFields);
-
-        const errors = $(".training-registration-form .error").length;
         
-        if (errors === 0) {
+        if ($(".training-registration-form .error").length === 0) {
+            const obj = $(this).serializeJSON({useIntKeysAsArrayIndex: true});
+            const jsonString = JSON.stringify(obj);
+            
             $.ajax({
                 url: "entry/",
                 method: "POST",
-                contentType: "application/json",
-                data : JSON.stringify({
-                    // "training": 1,
-                    // "reporting_person": {
-                    //   "name": "aneta",
-                    //   "email": "aneta@example.com",
-                    //   "phone_number": "100500600"
-                    // },
-                    // "reporting_person_is_participating": true,
-                    // "participants": [
-                    //   {
-                    //     "name": "string",
-                    //     "email": "a@example.com",
-                    //     "phone_number": "string"
-                    //   }
-                    // ],
-                    // "invoice": {
-                    //   "institution_name": "string",
-                    //   "institution_address": "string",
-                    //   "phone_number": "string",
-                    //   "email": "user@example.com",
-                    //   "nip": 0
-                    // }
-                  })
+                contentType: "application/json; charset=utf-8",
+                data: jsonString
             }).done(function () {
                 fields.val("").css("borderColor", "#ccc");
 
                 $(".registration-form-state").remove();
                 const $state = $(`<p class="registration-form-state">Dziękujemy za zapisanie się na szkolenie</p>`);
                 containerBtn.append($state);
-
                 btn.addClass("registration-btn-success").prop("disabled", true);
 
                 setTimeout(function(){
